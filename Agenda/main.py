c97667eeb76bed35cb1a5e0b62214b70313d813a
sordello andrea <sordello.andrea@gmail.com>
2023-04-27 11:38:24 +0200

    .

--- Agenda/main.py
+++ Agenda/main.py
@@ -1,5 +1,72 @@
+from operator import itemgetter
+
+def leggi_agenda(nome_file):
+    input_file= open(nome_file,"r",encoding="UTF_8")
+    print(input_file)
+    righe= input_file.readlines()
+    #print(righe)
+    agenda=[]
+    
+    for riga in righe:
+        riga=riga.rstrip()
+        campi=riga.split(";")
+        #print(campi)
+        evento={
+            "giorno":campi[0],
+            "ora": int(campi[1]),
+            "desc": campi[2]
+        }
+        #print(evento)
+        agenda.append(evento)
+    return agenda
+def visualizza_giorno(giorno,agenda):
+    listaVisualizza=[]
+    for evento in agenda:
+        if evento["giorno"]==giorno:
+            #print(evento)
+            listaVisualizza.append(evento)
+    listaVisualizza.sort(key=itemgetter("ora"))
+        #print(listaVisualizza)
+    for elemento in listaVisualizza:
+         print(f"giorno {elemento['giorno']} ora {elemento['ora']:02d}: {elemento['desc']}")
+
+def leggi_comandi(nome_file_comandi,agenda):
+    input_file_comandi=open(nome_file_comandi,"r",encoding="UTF-8")
+    righe= input_file_comandi.readlines()
+    #print(righe)
+    for riga in righe:
+        riga=riga.rstrip()
+        campi=riga.split(" ",maxsplit=3)
+
+        #print(campi)
+        if campi[0]=='v':
+            #visualizza
+            visualizza_giorno(campi[1],agenda)
+        else:
+                #inserisci
+           # print("inserisci")
+            trovato=False
+            for evento in agenda:
+                if evento["giorno"]== campi[1] and evento["ora"]==int(campi[2]):
+                    trovato=True
+
+            if trovato==False:
+                #inserisco
+                print("Appuntamento inserito correttamente")
+                eventoNuovo={
+                    "giorno":campi[1],
+                    "ora":int(campi[2]),
+                    "desc":campi[3]
+                }
+                agenda.append(eventoNuovo)
+                visualizza_giorno(campi[1],agenda)
+            else:
+                #impossibile inserire
+                print("Impossibile inserire appuntamento")
+
 
 def main():
-    pass
-
+      agenda=leggi_agenda("agenda.txt")
+      leggi_comandi("comandi.txt",agenda)
 main()
+
--- Costi_stampa/main.py
+++ Costi_stampa/main.py
@@ -1,4 +1,66 @@
+
+
+from operator import itemgetter
+
+
 def main():
-    pass
+    infile = open("costipagine.txt","r",encoding="UTF-8")
+   # print(infile)
+    righe= infile.readlines()
+    infile.close()
+    #print(righe)
+    listino=[]
+    for riga in righe:   #for i in range(0,len(righe))
+        #print(riga)
+        riga=riga.rstrip()
+
+        campi = riga.split(";")
+        #print(campi)
+        fascia={
+            "n_min" : int(campi[0]),
+            "n_max" : int(campi[1]),
+            "prezzo" : float(campi[2].rstrip("€")),
+        }
+        listino.append(fascia)
+       # print(fascia)
+    #print(listino)
+
+    listino.sort(key=itemgetter('n_min'))   #listino.sort('n_min')
+   # print(listino)
+    print("LISTINO PREZZI")
+    for elemento in listino:
+        print(f"Fino a {elemento['n_max']} pagine :  {elemento['prezzo']} €/pagina")
+
+    print("Inserisci nome file libri: ")
+    nome_file=input()
+    file_libri=open(nome_file,"r",encoding="UTF-8")
+    righe_libri= file_libri.readlines()
+    #print(righe_libri)
+    elenco_libri=[]
+    for riga_libri in righe_libri:
+        riga_libri=riga_libri.rstrip()
+        campi = riga_libri.split(";")
+        libro={
+            "titolo":campi[0],
+            "n_pag":int(campi[1])
+        }
+        #print(libro)
+        elenco_libri.append(libro)
+   #print(elenco_libri)
+
+    elenco_libri.sort(key=itemgetter("titolo"))
+    print("COSTI STAMPA")
+    totale=0
+    for libro in elenco_libri:
+        for fascia in listino:
+            if libro["n_pag"] >= fascia["n_min"] and libro["n_pag"]<= fascia["n_max"]:
+
+                costolibro= fascia["prezzo"] * libro["n_pag"]
+               #print(libro["titolo"]," ", costolibro)
+                print(f"{libro['titolo']} - Pagine: {libro['n_pag']} - Costo {costolibro:.2f} €")
+                totale += costolibro
+    print(f"Totale : {totale:.2f}")
+    file_libri.close()
+
 
 main()
