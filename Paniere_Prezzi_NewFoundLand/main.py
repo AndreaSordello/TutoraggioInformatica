2c7e5f07856ba90f36274bc4c35dd222286a8b10
sordello andrea <sordello.andrea@gmail.com>
2023-05-18 09:37:34 +0200

    .

--- Paniere_Prezzi_NewFoundLand/main.py
+++ Paniere_Prezzi_NewFoundLand/main.py
@@ -1,4 +1,51 @@
+def leggi_shop(nome_file):
+    input_file = open(nome_file,'r',encoding='UTF-8')
+    lista_negozi=[]
+    for linea in input_file:
+        linea= linea.rstrip()
+        lista_negozi.append(linea)
+    print(lista_negozi)
+    input_file.close()
+    return lista_negozi
+
+def leggi_rilevazioni(nome_file):
+    input_file = open(nome_file,'r',encoding='UTF-8')
+
+    input_file.readline()
+    lista_rilevazioni=[]
+    for linea in input_file:
+        linea=linea.rstrip()
+        campi = linea.split(',')
+        #print(campi)
+        if campi[4]=='E':
+            #prodotto essenziale
+            prodotto={
+                'negozio':campi[2],
+                'nome':campi[3],
+                'prezzo':float(campi[5])
+            }
+            lista_rilevazioni.append(prodotto)
+    print(lista_rilevazioni)
+    input_file.close()
+    return lista_rilevazioni
+def stampa_essenziali(l_r,l_n):
+    lista_elementi_stampa=[]        # Apple,Tomato,
+
+    for prodotto in l_r:
+        if  prodotto['nome'] not in lista_elementi_stampa:
+            if prodotto['negozio'] in l_n:      #controllo se è un prodotto venduto dai negozi che sto controllando
+                lista_elementi_stampa.append(prodotto['nome'])
+
+    lista_elementi_stampa.sort() # ordine alfabetico/ordine crescente
+    print("Prodotti:")
+    for p in lista_elementi_stampa:
+        print(f"- {p}")         # print ("-",p)
+    #print(lista_elementi_stampa)
+
 def main():
-    pass
+   lista_negozi=leggi_shop('shop.txt')
+   lista_rilevazioni = leggi_rilevazioni('NLFoodPricing.csv')
+
+   stampa_essenziali(lista_rilevazioni,lista_negozi)
 
 main()
