42f97059687351d2b998b7bad7f8f57830e06fbd
sordello andrea <sordello.andrea@gmail.com>
2023-05-18 11:19:53 +0200

    .

--- GestioneContoRistorante/main.py
+++ GestioneContoRistorante/main.py
@@ -1,4 +1,57 @@
+def leggi_menu(nome_file):
+    input_file=open(nome_file,'r',encoding='UTF-8')
+    menu=[]
+    for linea in input_file:
+        linea=linea.rstrip()
+       #print(linea)
+        campi=linea.split(',')
+        piatto =  {
+            'ID':campi[0],
+            'nome':campi[1],
+            'prezzoConIVA': float(campi[2]),
+            'percIVA': int(campi[3])
+        }
+        menu.append(piatto)
+    input_file.close()
+    return menu
+def leggi_ordine(nome_file):
+    input_file=open(nome_file,'r',encoding='UTF-8')
+    ordine=[]
+    for linea in input_file:
+        linea=linea.rstrip()
+        campi=linea.split(',')
+        elemento={
+            'ID':campi[0],
+            'qt':int(campi[1]),
+        }
+        ordine.append(elemento)
+    input_file.close()
+    return ordine
+
+def calcolaRicevuta(menu,ordine):
+
+    ricevuta=[]
+    for o in ordine:
+        for m in menu:
+            if o['ID']==m['ID']:
+               #per ogni ordine calcolare i costi tenendo conto dell'iva
+                costoOrdineConIva=o['qt']*m['prezzoConIVA']
+                IVAordine = (m['prezzoConIVA'] * m['percIVA']/100) * o['qt']
+                ordineCalcolato={
+                    'nome':m['nome'],
+                    'qt':o['qt'],
+                    'costoOrdineConIva':costoOrdineConIva,
+                    'IVAordine':IVAordine,
+                    'percIVA':m['percIVA']
+                }
+                ricevuta.append(ordineCalcolato)
+    print(ricevuta)
+
 def main():
-    pass
+    menu=leggi_menu('menu.txt')
+    ordine=leggi_ordine('ordine.txt')
+    print(menu)
+    print(ordine)
+    calcolaRicevuta(menu,ordine)
 
 main()
--- Paniere_Prezzi_NewFoundLand/main.py
+++ Paniere_Prezzi_NewFoundLand/main.py
@@ -1,3 +1,6 @@
+from operator import itemgetter
+
+
 def leggi_shop(nome_file):
     input_file = open(nome_file,'r',encoding='UTF-8')
     lista_negozi=[]
@@ -42,10 +45,65 @@ def stampa_essenziali(l_r,l_n):
         print(f"- {p}")         # print ("-",p)
     #print(lista_elementi_stampa)
 
+def stampa_prezzi_minimi(l_r,l_n):
+    l_n.sort()
+
+    for negozio in l_n:
+        lista_prodotti_negozio=[]    #lista di dizionari con negozio,nome,prezzo
+        for prodotto in l_r:
+            if prodotto['negozio']==negozio:
+                #prodotto venduto da quel negozio
+               # print(prodotto)
+                #devo fare controlli, esiste già?il prezzo memorizzato è il minimo?
+                trovato=False
+                for prodotto_negozio in lista_prodotti_negozio:
+                    if prodotto_negozio['nome']==prodotto['nome']:
+                        #esiste già
+                        trovato=True
+                        #il prezzo memorizzato è il minimo?
+                        if prodotto_negozio['prezzo']>prodotto['prezzo']:
+                            #devo memorizzare il nuovo prodotto perchè costa di meno
+                            # e buttare via il prodotto vecchio
+                            lista_prodotti_negozio.append(prodotto)
+                            lista_prodotti_negozio.remove(prodotto_negozio)
+                if not trovato:
+                    lista_prodotti_negozio.append(prodotto)
+        #print(lista_prodotti_negozio)
+        print(negozio)
+        lista_prodotti_negozio.sort(key=itemgetter('nome'))
+        for prodotto_negozio in lista_prodotti_negozio:
+            print(f"- {prodotto_negozio['nome']} : {prodotto_negozio['prezzo']} $/chilo")
+
+def trova_minimo(l_r,l_n,prodotto):
+
+    #min(l_r,key=itemgetter('prezzo'))
+    lista_prezzi=[]
+    for rilevazione in l_r:
+        if rilevazione['nome']==prodotto and rilevazione['negozio'] in l_n:
+            lista_prezzi.append(rilevazione)
+    #print(lista_prezzi)
+    if len(lista_prezzi)==0:
+        print("Prodotto non disponibile")
+    else:
+        minimo=min(lista_prezzi,key=itemgetter('prezzo'))
+        print(f"Prezzo minimo {minimo['prezzo']} $/chilo da {minimo['negozio']}")
+
+
 def main():
    lista_negozi=leggi_shop('shop.txt')
    lista_rilevazioni = leggi_rilevazioni('NLFoodPricing.csv')
 
    stampa_essenziali(lista_rilevazioni,lista_negozi)
+   stampa_prezzi_minimi(lista_rilevazioni,lista_negozi)
+
+   print("Che cibo vuoi cercare:")
+   prodotto=input()
+   while ( prodotto!='q'):
+        trova_minimo(lista_rilevazioni,lista_negozi,prodotto)
+        print("Che cibo vuoi cercare:")
+        prodotto=input()
+
+
+   print("Arrivederci")
 
 main()
