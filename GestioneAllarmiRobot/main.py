7d4491bc184e899af097e8a3fe494cc11a0ff6f2
sordello andrea <sordello.andrea@gmail.com>
2023-06-08 09:08:41 +0200

    .

--- GestioneAllarmiRobot/main.py
+++ GestioneAllarmiRobot/main.py
@@ -1,4 +1,23 @@
+def leggi_allarmi(nome_file):
+    input_file=open(nome_file,'r',encoding='UTF-8')
+
+    input_file.readline() #legge l'intestazione
+    lista_allarmi=[]
+    for linea in input_file:
+        linea=linea.rstrip()
+        campi=linea.split(';')
+       #print(campi)
+        allarme={
+            'id':campi[0],
+            'sev':int(campi[1])
+        }
+        lista_allarmi.append(allarme)
+    print(lista_allarmi)
+    return lista_allarmi
+
+
 def main():
-    pass
+
+    lista_allarmi=leggi_allarmi('allarmi.csv')
 
 main()
