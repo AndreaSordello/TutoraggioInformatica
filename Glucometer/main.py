245c4a37db9be9de4290dbbf0ba53c3c925f3fd2
sordello andrea <sordello.andrea@gmail.com>
2023-06-12 18:22:18 +0200

    .

--- Glucometer/main.py
+++ Glucometer/main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from operator import itemgetter
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def leggi_glucometers(nome_file):
+    input_file = open(nome_file,"r",encoding="UTF-8")
 
+    lista_pazienti=[]
+    for linea in input_file:
+        linea =linea.rstrip()
+        campi= linea.split(" ")
+       # print(campi)
+        if int(campi[2])>=200:
+            #verificato superamento
+            #il paziente è già in lista?
+            trovato=False
+            for paziente in lista_pazienti:
+                if paziente["ID"]==campi[0]:
+                    #trovato paziente
+                    trovato=True
+                    paziente["ListaOra"].append(campi[1])
+                    paziente["ListaGlicemia"].append(int(campi[2]))
+                    paziente["cont"]+=1
+            if not trovato:
+                #non ho ancora letto quel paziente
+                paziente={
+                    "ID":campi[0],
+                    "ListaOra":[],
+                    "ListaGlicemia":[],
+                    "cont":0
+                }
+                paziente["ListaOra"].append(campi[1])
+                paziente["ListaGlicemia"].append(int(campi[2]))
+                paziente["cont"]+=1
+                lista_pazienti.append(paziente)
+   # print(lista_pazienti)
+    input_file.close()
+    return lista_pazienti
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def stampa_pazienti(lista_pazienti):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    lista_pazienti.sort(key=itemgetter("cont"),reverse=True)
+  #  print(lista_pazienti)
+    for paziente in lista_pazienti:
+            for i in range(0,paziente["cont"]):
+                print(f"{paziente['ID']} {paziente['ListaOra'][i]} {paziente['ListaGlicemia'][i]} ")
+            print("")
+def main():
+   lista_pazienti=leggi_glucometers("glucometers.txt")
+
+   stampa_pazienti(lista_pazienti)
+
+main()
