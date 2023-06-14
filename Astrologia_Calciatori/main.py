b4b19f06bc0c261397547a0248f2f065783012ee
sordello andrea <sordello.andrea@gmail.com>
2023-06-14 18:56:02 +0200

    .

--- Astrologia_Calciatori/main.py
+++ Astrologia_Calciatori/main.py
@@ -1,16 +1,75 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from operator import itemgetter
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def leggi_zodiaco(nome_file):
+    input_file = open(nome_file,"r",encoding="UTF-8")
+    lista_zodiaco=[]
+    for linea in input_file:
+        linea = linea.rstrip()
+        campi = linea.split(",")
+       # print(campi)
+        data = campi[1].split("/")
+        dataInizio = data[1] + data[0]
+        data = campi[2].split("/")
+        dataFine = data[1]+data[0]
+
+        zodiaco = {
+            "segno":campi[0],
+            "inizio":dataInizio,
+            "fine":dataFine,
+            "count":0,
+        }
+        lista_zodiaco.append(zodiaco)
+    input_file.close()
+    #print(lista_zodiaco)
+    return lista_zodiaco
+
+def leggi_calciatori(nome_file,lista_zodiaco):
+    input_file = open(nome_file,"r",encoding="UTF-8")
+
+    for linea in input_file:
+        linea = linea.rstrip()
+        campi = linea.split(",")
+        gol = int(campi[1])
+
+       # print(campi[3])
+        data = campi[3].split("/")
+        dataNascita= data[1] + data[0]
+        #print(dataNascita)
+
+       # assegnato=False   #se si cerca il capricorno
+        for segno in lista_zodiaco:
+            if dataNascita>= segno["inizio"] and dataNascita<=segno["fine"]:
+               # print(segno["segno"])
+                #HO TROVATO IL SEGNO
+                segno["count"] += gol
+              #  assegnato=True
+            if segno["segno"]=="Capricorno":
+                if dataNascita<=segno["fine"] or dataNascita>=segno["inizio"]:
+                   # print(segno["segno"])
+                    #HO TROVATO IL SEGNO
+                    segno["count"] += gol
+         #if assegnato==False:
+        #    print("Assegnato capricorno")
+    #print(lista_zodiaco)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    lista_zodiaco.sort(key=itemgetter("count"),reverse=True)
+    #num_max : 50 = num_gol : num_asterischi
+    #num_asterischi = 50*num_gol/num_max
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    fattoreRiscalamento = 50 /lista_zodiaco[0]["count"]
+
+    for segno in sorted(lista_zodiaco,key=itemgetter("count"),reverse=True):
+
+
+        print(f"{segno['segno']:<10} ({segno['count']})",end=" ")
+        print("*"*round(segno["count"]*fattoreRiscalamento))
+
+
+
+
+def main():
+    lista_zodiaco=leggi_zodiaco("zodiaco.csv")
+    leggi_calciatori("sportivi.csv",lista_zodiaco)
+main()
--- Mezzi_Pubblici/main.py
+++ Mezzi_Pubblici/main.py
@@ -1,16 +1,121 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from operator import itemgetter
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def leggi_database(nome_file):
+
+    input_file = open(nome_file,"r",encoding="UTF-8");
+
+    database={}
+    for linea in input_file:
+        linea = linea.rstrip()
+        campi =linea.split(": ")
+        database[campi[0]]=campi[1]
+    input_file.close()
+   # print(database)
+    return database
+
+def leggi_dati(nome_file):
+    input_file = open (nome_file,"r",encoding="UTF-8")
+
+    intestazione = input_file.readline()
+    intestazione = intestazione.rstrip()
+    campi = intestazione.split(",")
+   # print(campi)
+    lista_fermate = campi[2:]
+   # print(lista_fermate)
+
+    lista_tratte=[]
+    for linea in input_file:
+        linea = linea.rstrip()
+        campi = linea.split(",")
+        tratta={
+            "IDautista":campi[0],
+            "fermate":campi[2:]
+        }
+        lista_tratte.append(tratta)
+    input_file.close()
+    return lista_fermate,lista_tratte
+
+def conta_saliti_scesi(lista_tratte,lista_fermate,db):
+
+    lista_saliti_scesi=[]
+    for i in range(0,len(lista_fermate)):
+        count_saliti=0
+        count_scesi =0
+        for tratta in lista_tratte:
+            passeggeri = tratta["fermate"][i]
+           # print(passeggeri)
+            #CASO -- , n , -n , n/m
+
+            if "/" in passeggeri:
+                campi = passeggeri.split("/")
+                count_saliti += int(campi[0])
+                count_scesi += int(campi[1])
+            else:
+                if passeggeri != "--":
+                    # caso n , -n
+                    if passeggeri.isnumeric():
+                        count_saliti+= int(passeggeri)
+                    else:
+
+                        count_scesi += abs(int(passeggeri))
+
+        lista_saliti_scesi.append({
+            "IDfermata":lista_fermate[i],
+            "saliti":count_saliti,
+            "scesi":count_scesi
+        })
+   # print(lista_saliti_scesi)
+
+    massimo = max(lista_saliti_scesi,key=itemgetter("saliti"))
+    print(f"La fermata in cui salgono più passeggeri ({massimo['saliti']}) è {db[massimo['IDfermata']]}")
+    massimo = max(lista_saliti_scesi,key=itemgetter("scesi"))
+    print(f"La fermata in cui scendono più passeggeri ({massimo['scesi']}) è {db[massimo['IDfermata']]}")
+
+def conta_saliti_autista(lista_tratte,db):
+
+    diz_autista ={}
+    for tratta in lista_tratte:
+        count_pass = 0
+        for passeggeri in tratta["fermate"]:
+                if "/" in passeggeri:
+                    campi = passeggeri.split("/")
+                    count_pass += int(campi[0])
+                else:
+                    if passeggeri != "--":
+                        # caso n , -n
+                        if passeggeri.isnumeric():
+                            count_pass+= int(passeggeri)
+        if tratta["IDautista"] in diz_autista.keys():
+            diz_autista[tratta["IDautista"]] +=count_pass
+        else:
+            diz_autista[tratta["IDautista"]] =count_pass
+    #print(diz_autista)
+
+    lista_autista=[]
+    for chiave in diz_autista.keys():
+        autista={
+            "nome":db[chiave],
+            "count": diz_autista[chiave]
+        }
+        lista_autista.append(autista)
+   # print(lista_autista)
+    lista_autista.sort(key=itemgetter("count"),reverse=True)
+   # print(lista_autista)
+    for autista in lista_autista:
+        print(f"{autista['nome']} con {autista['count']}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def main():
+
+    db = leggi_database("database.txt")
+
+    lista_f,lista_t= leggi_dati("dati_aeroporto_torino.csv")
+
+    #print(lista_f)
+    #print(lista_t)
+
+    conta_saliti_scesi(lista_t,lista_f,db)
+    conta_saliti_autista(lista_t,db)
+main()
