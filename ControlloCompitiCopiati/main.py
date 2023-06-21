523077b80a48719b770033b1e68e083cbc1ec593
sordello andrea <sordello.andrea@gmail.com>
2023-06-21 17:14:44 +0200

    .

--- ControlloCompitiCopiati/main.py
+++ ControlloCompitiCopiati/main.py
@@ -1,16 +1,81 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def leggi_risposte(nome_file):
+    input_file = open (nome_file,"r",encoding="UTF-8")
+
+    diz_risposte ={}
+
+    for linea in input_file:
+        linea = linea.rstrip()
+        campi = linea.split(" ")
+       # print(campi)
+        diz_risposte[campi[0]] = campi[1:]
+    print(diz_risposte)
+    input_file.close()
+    return diz_risposte
+
+def leggi_posizioni(nome_file):
+    input_file = open(nome_file,"r",encoding="UTF-8")
+    matrice_posizioni=[]
+    for linea in input_file:
+        linea = linea.rstrip()
+        campi = linea.split(" ")
+        #print(campi)
+        matrice_posizioni.append(campi)
+    print(matrice_posizioni)
+    input_file.close()
+    return matrice_posizioni
+
+def verificaRisposte(studenteA,studenteB,listaRisposteA,listaRisposteB):
+
+    #caso risposte completamente uguali
+    if listaRisposteA==listaRisposteB:
+        print(f"le risposte di {studenteA} e {studenteB} sono uguali")
+
+    copiatoDa=False
+    isDifferente=False
+    for i in range(0,len(listaRisposteA)):
+            if listaRisposteA[i] != listaRisposteB[i]:
+                if listaRisposteA[i] =="-" and listaRisposteB[i]!="-":
+                    copiatoDa=True
+                else:
+                    isDifferente=True
+
+    if copiatoDa and isDifferente==False:
+        print(f"{studenteA} può aver copiato da {studenteB}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def controllaCopiature(matrice_posizioni,diz_risposte):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for i in range(0,len(matrice_posizioni)):
+        for j in range(0,len(matrice_posizioni[i])):
+
+            studenteA=matrice_posizioni[i][j]
+            listaRisposteA=diz_risposte[studenteA]
+           # print(studenteA,listaRisposteA)
+            if j-1 >=0:
+                #matrice_posizioni[i][j-1] VICINO DI SX
+                studenteB=matrice_posizioni[i][j-1]
+                listaRisposteB=diz_risposte[studenteB]
+              #  print(studenteB,listaRisposteB)
+                verificaRisposte(studenteA,studenteB,listaRisposteA,listaRisposteB)
+
+            if j+1 < len(matrice_posizioni[i]):
+                studenteB=matrice_posizioni[i][j+1]
+                listaRisposteB=diz_risposte[studenteB]
+              #  print(studenteB,listaRisposteB)
+                verificaRisposte(studenteA,studenteB,listaRisposteA,listaRisposteB)
+           # print()
+
+
+            #matrice_posizioni[i][j+1]
+
+
+
+def main():
+    diz_risposte = leggi_risposte("risposte.txt")
+    matrice_posizioni=leggi_posizioni("posizioni.txt")
+
+    controllaCopiature(matrice_posizioni,diz_risposte)
+
+main()
