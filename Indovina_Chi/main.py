cd6c18e6cadf5b87fbfa10f759bd653ca894cfb1
sordello andrea <sordello.andrea@gmail.com>
2023-06-26 17:13:51 +0200

    .

--- Indovina_Chi/main.py
+++ Indovina_Chi/main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def leggi_personaggi(nome_file):
+    input_file = open(nome_file,"r",encoding='UTF-8')
 
+    intestazione = input_file.readline()
+    intestazione = intestazione.rstrip()
+    campiIntestazione = intestazione.split(";")
+   # print(campiIntestazione)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    lista_personaggi=[]
+    for linea in input_file:
+        linea = linea.rstrip()
+        campi = linea.split(";")
+       # print(campi)
+        personaggio={}
+        for i in range(0,len(campi)):
+            personaggio[campiIntestazione[i]]=campi[i]
+        #print(personaggio)
+        lista_personaggi.append(personaggio)
+   # print(lista_personaggi)
+    input_file.close()
+    return lista_personaggi
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def leggi_applica_domande(nome_file,lista_personaggi):
+    input_file = open(nome_file,"r",encoding="UTF-8")
+    contatore = 1
+    for linea in input_file:
+        linea = linea.rstrip()
+        campi = linea.split("=")
+        domanda = campi[0]
+        risposta = campi[1]
+      #  domanda,risposta = linea.split("=")
+        print(f"\nMossa {contatore} - domanda:{domanda} = {risposta}")
+        contatore+=1
+        #gestione della domanda letta
+        lista_personaggi_filtrata=[]
+        for personaggio in lista_personaggi:
+            if personaggio[domanda] == risposta:
+                #lista_personaggi.remove(personaggio)
+                lista_personaggi_filtrata.append(personaggio)
+
+        #print(lista_personaggi_filtrata)
+        lista_personaggi=lista_personaggi_filtrata
+        for personaggio in lista_personaggi:
+            stampa_personaggio(personaggio)
+    if len(lista_personaggi) == 1:
+        print("\nGioco terminato, hai vinto ! il personaggio selezionato è")
+        stampa_personaggio(lista_personaggi[0])
+    else:
+        print("\nPeccato, hai perso :-(")
+
+def stampa_personaggio(personaggio):
+    print(f"{personaggio['Nome']} - ",end="" )
+    for chiave in personaggio.keys():
+        if chiave != "Nome":
+            print(f"{chiave}:{personaggio[chiave]}, ",end ="")
+    print()
+
+def main():
+      lista_personaggi=leggi_personaggi("personaggi.txt")
+      print("Personaggi del gioco:")
+      for personaggio in lista_personaggi:
+          stampa_personaggio(personaggio)
+
+      leggi_applica_domande("domande1.txt",lista_personaggi)
+
+main()
