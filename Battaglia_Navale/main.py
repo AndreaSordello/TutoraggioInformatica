c38d4d1b8cc77beee3bffcca641669596bbccf7f
sordello andrea <sordello.andrea@gmail.com>
2023-09-12 11:47:59 +0200

    .

--- Battaglia_Navale/main.py
+++ Battaglia_Navale/main.py
@@ -1,16 +1,112 @@
-# This is a sample Python script.
+#lettere ={"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9}
+lettere =["A","B","C","D","E","F","G","H","I","J"]
+def read_data(nome_file):
+    file = open (nome_file,"r",encoding="UTF-8")
+    linee = file.readlines()
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    tabella = []
+    for linea in linee:
+        linea = linea.rstrip()
+        celle = linea.split(",")
+        tabella.append(celle)
+
+    file.close()
+    return tabella
+
+def leggi_mosse(nome_file):
+    file = open (nome_file,"r",encoding="UTF-8")
+    linee = file.readlines()
+    lista=[]
+    for linea in linee:
+        linea = linea.rstrip()
+        campi = linea.split(",")
+
+        d = {
+            "r": campi[0],
+            "c": campi[1]
+        }
+        lista.append(d)
+
+    file.close()
+    return lista
+
+def tabella_vuota():
+    tab = []
+    for i in range(0,10):
+        riga=[]
+        for j in range(0,10):
+            riga.append('')
+        tab.append(riga)
+    return tab
+
+def playround(tab_game,mossa,tab_navi):
+   # lettere =["A","B","C"]
+   # r= lettere.index("A")
+
+    r = lettere.index(mossa["r"])
+    c = int(mossa["c"])-1
+
+    if tab_navi[r][c] != '':
+        print("Colpito")
+        tab_game[r][c] = "*"
+    else:
+        print("Acqua")
+        tab_game[r][c] = "o"
+
+def stampa_matrice(tabella):
+    print("  |",end="")
+    for i in range(1,11):
+        print(f"{i:2}|",end="")
+    print()
+    for j in range(0,11):
+            print("___",end="")
+    print()
+    for i in range (0,10):
+        print(f"{lettere[i]} |",end="")
+        for j in range(0,10):
+            print(f"{tabella[i][j]:2}|",end="")
+        print()
+        for j in range(0,11):
+            print("___",end="")
+        print()
+
+def check_affondate(tab_game,tab_navi):
+    for i in range(0,10):
+        for j in range(0,10):
+            if tab_navi[i][j] != '' and tab_game[i][j] == "":
+                return False
+    return True
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
 
+    tab1 = read_data("nave1.txt")
+    tab2 = read_data("nave2.txt")
+    mosse= leggi_mosse("mosse.txt")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    tab1_game =tabella_vuota()
+    tab2_game =tabella_vuota()
+    #print(tab1_game)
+    i = 0
+    fine = False
+    while i <len(mosse) and not fine:
+        giocatore = (i % 2)+1
+        print("E' il turno del giocatore",giocatore)
+        print(f"Coordinate dell'attacco: {mosse[i]['r']} , {mosse[i]['c']}")
+        if giocatore == 1:
+            playround(tab2_game,mosse[i],tab2)
+            stampa_matrice(tab2_game)
+            if check_affondate(tab2_game,tab2):
+                print("Giocatore 1 ha vinto")
+                fine =True
+        else:
+            playround(tab1_game,mosse[i],tab1)
+            stampa_matrice(tab1_game)
+            if check_affondate(tab1_game,tab1):
+                print("Giocatore 2 ha vinto")
+                fine=True
+        i+=1
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if fine == False:
+        print("partita sospesa")
+main()
--- Capitan_Nemo/main.py
+++ Capitan_Nemo/main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from operator import itemgetter
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def leggi_viaggi(nome_file):
+    file = open(nome_file,"r",encoding="UTF-8")
+    lista=[]
+    for linea in file:
+        linea=linea.rstrip()
+        campi = linea.split(",")
+        d = {
+            "luogo":campi[0],
+            "durata": int(campi[1]),
+            "passeggeri": int(campi[2])
+        }
+        lista.append(d)
+    #print(lista)
+    file.close()
+    return lista
+def leggi_pietre(nome_file):
+    file = open(nome_file,"r",encoding="UTF-8")
+    diz_pietre={}
+    for linea in file:
+        linea = linea.rstrip()
+        campi = linea.split(",")
+        #print(campi)
+        diz_pietre[campi[0]] = campi[1:]
+    #print(diz_pietre)
+    file.close()
+    return diz_pietre
+def main():
+    viaggi=leggi_viaggi("viaggi_nemo.txt")
+    pietre = leggi_pietre("pietre_preziose_luoghi.txt")
+
+    sumPasseggeri =0
+    sumDurata=0
+    for viaggio in viaggi:
+        sumPasseggeri += viaggio["passeggeri"]
+       #sumPasseggeri = sumPasseggeri + viaggio["passeggeri"]
+        sumDurata += viaggio["durata"]
+    media= sumDurata /len(viaggi)
+    print(f"Durata media dei viaggi:{media:.1f}")
+    print("Numero totale di passeggeri:",sumPasseggeri)
+    print("Tipi di pietre preziose per luogo visitato:")
+    diz_occorrenze={}
+    for viaggio in viaggi:
+
+        elenco = pietre[viaggio["luogo"]]
+
+        stringa= ", ".join(elenco)
+        print(f"- {viaggio['luogo']}: {stringa}")
+        for pietra in elenco:
+            if pietra not in diz_occorrenze:
+                diz_occorrenze[pietra]=1
+            else:
+                diz_occorrenze[pietra]+=1
+    #print(diz_occorrenze)
+
+    lista=[]
+    for elem in diz_occorrenze:
+        lista.append({
+            "pietra":elem,
+            "occorrenza":diz_occorrenze[elem]
+        })
+    #print(lista)
+
+    lista.sort(key=itemgetter("occorrenza"),reverse=True)
+    #print(lista)
+    comuni=[]
+    for pietra in lista[0:3]:
+        comuni.append(pietra["pietra"])
+    #print(comuni)
+
+    print("I tre tipi di pietre preziose più comuni:",", ".join(comuni))
+
+main()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
